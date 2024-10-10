4ffaa68edc01f5716b9c66ddbacbb5ba1faa774f
River0909 <a.feliipe09rs@gmail.com>
2024-10-09 19:53:34 -0500

    se agrego desde el ejercicio 25 al ejercicio 28

---
+++ proyecto/config/sidebar.php
@@ -0,0 +1,68 @@
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+          <img src="img/logo/lp4.png">
+        </div>
+        <div class="sidebar-brand-text mx-3">mi proyecto</div>
+      </a>
+     
+      <div class="sidebar-heading">
+        imagenes
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+          aria-expanded="true" aria-controls="collapseBootstrap">
+          <i class="fas fa-regular fa-image"></i>
+          <span>Imagenes 1</span>
+        </a>
+        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Bootstrap UI</h6>
+           
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"
+          aria-controls="collapseForm">
+          <i class="fas fa-regular fa-image"></i>
+          <span>Imagenes 2</span>
+        </a>
+       
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+          aria-controls="collapseTable">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Historias</span>
+        </a>
+        <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Información
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true"
+          aria-controls="collapsePage">
+          <i class="fas fa-fw fa-columns"></i>
+          <span>Información personal</span>
+        </a>
+        <div id="collapsePage" class="collapse" aria-labelledby="headingPage" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Example Pages</h6>
+            <a class="collapse-item" href="login.html">Login</a>
+            <a class="collapse-item" href="register.html">Register</a>
+            <a class="collapse-item" href="404.html">404 Page</a>
+            <a class="collapse-item" href="blank.html">Blank Page</a>
+          </div>
+        </div>
+      </li>
+    
+    </ul>
