0bfa4d81c8f339b9c6a0bd501029815d665311da
pablangas28 <pablitoerira28@gmail.com>
2026-02-19 21:09:26 -0500

    Se agrego el proyecto laravel con su base de datos y agregamos el challengue de routes

---
+++ 19-laravel/app/Models/Adoption.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Adoption extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'pet_id',
+    ];
+
+    //Relationship
+    //User has many Adoptions
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    //Relationship
+    //User has many Adoptions
+    public function pet(){
+        return $this->belongsTo(Pet::class);
+    }
+}
---
+++ 19-laravel/app/Models/Pet.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'image',
+        'kind',
+        'weight',
+        'age',
+        'breed',
+        'location',
+        'description',
+        'active',
+        'status'
+    ];
+
+    //Relationship
+    //User has many Adoptions
+    public function adoption(){
+        return $this->hasOne(Adoption::class);
+    }
+}
